c4a8c89012798d86f8c41d0e86874abd400ee6db
LázaroK <lazarolk23@hotmail.com>
2025-08-24 16:36:25 -0300

    feat: adiciona resolução do exercicio 8

---
+++ resolucoes/exercicios/exercicio08/src/main/program/exercicio8/Exercicio8.java
@@ -0,0 +1,25 @@
+package exercicio8;
+
+import java.util.Scanner;
+
+public class Exercicio8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Vamos verificar se um número é dívisível por um divisor?");
+        System.out.println("Insira o número que será o divisor: ");
+        int divisor = scan.nextInt();
+
+      //  boolean procurandoNumeroDivisivel = true;
+        System.out.println("Vamos procurar um número divisível por "+divisor);
+        while(true){
+            System.out.println("Insira um número: ");
+            int numero = scan.nextInt();
+            if (numero % divisor == 0){
+                System.out.println(numero+" é divisível por "+divisor);
+               //procurandoNumeroDivisivel = true;
+                break;
+            }
+        }
+
+    }
+}
